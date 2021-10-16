0e32ad53ededd23ff0936ce9d0c79f6e8daef75d
kant_123 <kanta@gmail.com>
2021-10-16 06:03:07 +0000

    this is calculatin program

--- regi.java
+++ regi.java
@@ -1,8 +1,25 @@
-hi print(mani)
-what are you doing (printf)
-where are you from,,!!
-what do u do?
-tell me about yourself.
-what do you study?
-what about you*
-what do you want!!!!!
+# Program make a simple calculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+output
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4): 3
+Enter first number: 15
+Enter second number: 14
+15.0 * 14.0 = 210.0
+
