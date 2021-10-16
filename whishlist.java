771eed32c2f31cc8884e21f5547c5ac53a56bf47
kant_123 <kanta@gmail.com>
2021-10-16 05:48:08 +0000

    this is add two numbers program

--- whishlist.java
+++ whishlist.java
@@ -1,6 +1,17 @@
-print "welcome to Mani1"
+# print "welcome to Mani1"
+
 def personal_details():
     name, age = "mani", 23
     address = "hyderabad, telangana, india"
     print("name: {}\n age: {}\n address: {}" .format(name, age, address))
 personal_details()!!!
+
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+output
+the sum of 1.5 and 6.3 is 7.8
